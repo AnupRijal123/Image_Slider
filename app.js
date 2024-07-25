77f9a97962d10b8c8d8fd3df5db25577849662c4
Anup <anuprijal321@gmail.com>
2024-07-25 17:23:48 +0545

    added javascript file and written code to append when next clicked  and prepend when previous clicked

---
+++ app.js
@@ -0,0 +1,52 @@
+const previousButtonDom = document.querySelector('.previous-button');
+const nextButtonDom = document.querySelector('.next-button');
+const mainContainerDom = document.querySelector('.main-container ')
+const sliderDom = document.querySelector('.slider');
+
+
+nextButtonDom.onclick = function () {
+    changeCarouselImage('nextButton')
+}
+
+previousButtonDom.onclick = function () {
+    changeCarouselImage('previousButton');
+
+}
+
+
+
+const changeCarouselImage = function (buttonName) {
+    console.log(buttonName);
+
+    let carouselContainerDom = document.querySelector('.carousel-container');
+
+    let carouselItemDoms = document.querySelectorAll('.carousel-item')
+    let sliderItemDoms = document.querySelectorAll('.slider-item');
+
+
+    if (buttonName === 'nextButton') {
+        carouselContainerDom.appendChild(carouselItemDoms[0]);
+
+        sliderDom.appendChild(sliderItemDoms[0]);
+
+        mainContainerDom.classList.add('next');
+        setTimeout(() => {
+            mainContainerDom.classList.remove('next');
+        }, 5000);
+
+    }
+    if (buttonName === 'previousButton') {
+        const lastPosition = carouselItemDoms.length - 1;
+        carouselContainerDom.prepend(carouselItemDoms[lastPosition]);
+
+        sliderDom.prepend(sliderItemDoms[lastPosition]);
+
+        mainContainerDom.classList.add('previous');
+        setTimeout(() => {
+            mainContainerDom.classList.remove('previous');
+        }, 5000);
+    }
+
+
+
+}
